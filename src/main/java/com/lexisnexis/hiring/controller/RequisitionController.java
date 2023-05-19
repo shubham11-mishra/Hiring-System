6dbad3a4aa371fc426a44f839141e2d32e4a7158
Shubham Mishra <shubham.mishra@lexisnexisrisk.com>
2023-05-19 13:36:59 +0530

    Added Requisition Management with jwt

--- src/main/java/com/lexisnexis/hiring/controller/RequisitionController.java
+++ src/main/java/com/lexisnexis/hiring/controller/RequisitionController.java
@@ -11,6 +11,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.lexisnexis.hiring.entity.Requisition;
@@ -18,6 +19,7 @@ import com.lexisnexis.hiring.exception.InvalidEmployeeID;
 import com.lexisnexis.hiring.service.RequisitionService;
 
 @RestController
+@RequestMapping("/requistion")
 public class RequisitionController {
 
 	@Autowired
