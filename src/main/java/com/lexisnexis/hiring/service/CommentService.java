4b8990a7bc2c2f5114b5259464fdc35a8ebb0627
Ravikant Madas <75243383+ravikant-2004@users.noreply.github.com>
2023-05-22 11:50:10 +0530

    Added Soniya Comment module

--- src/main/java/com/lexisnexis/hiring/service/CommentService.java
+++ src/main/java/com/lexisnexis/hiring/service/CommentService.java
@@ -1,15 +1,13 @@
 package com.lexisnexis.hiring.service;
 
-import java.security.NoSuchAlgorithmException;
-import java.util.List;
-
 import com.lexisnexis.hiring.entity.Comments;
 import com.lexisnexis.hiring.exception.UserNotFoundException;
-import com.lexisnexis.hiring.response.APIResponse;
+
+import java.util.List;
 
 public interface CommentService {
 	
-	public APIResponse createComment(Comments comment) throws NoSuchAlgorithmException;
+	public Comments createComment(Comments comment) ;
 	
 	public List<Comments> getComments();
 	
@@ -19,7 +17,7 @@ public interface CommentService {
 	
 	public Comments getCommentsByCandidateId(int candidate_id);
 	
-	public APIResponse updateComment(Comments comment, int comment_id);
+	public Comments updateComment(Comments comment, int comment_id);
 	
 	public void deleteComment(int comment_id) throws UserNotFoundException;
 }
--- src/main/java/com/lexisnexis/hiring/service/impl/CommentServiceImpl.java
+++ src/main/java/com/lexisnexis/hiring/service/impl/CommentServiceImpl.java
@@ -1,6 +1,5 @@
 package com.lexisnexis.hiring.service.impl;
 
-import java.security.NoSuchAlgorithmException;
 import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Optional;
@@ -11,7 +10,6 @@ import org.springframework.stereotype.Service;
 import com.lexisnexis.hiring.entity.Comments;
 import com.lexisnexis.hiring.exception.UserNotFoundException;
 import com.lexisnexis.hiring.repository.CommentsRepository;
-import com.lexisnexis.hiring.response.APIResponse;
 import com.lexisnexis.hiring.service.CommentService;
 
 @Service
@@ -20,57 +18,39 @@ public class CommentServiceImpl implements CommentService{
 	@Autowired
 	CommentsRepository commentsRepository;
 
-	public APIResponse createComment(Comments comment) throws NoSuchAlgorithmException {
-		APIResponse apiResponse = new APIResponse();
-		commentsRepository.save(comment);
-		apiResponse.setData("Comment saved succesfully");
-
-		return apiResponse;
+	public Comments createComment(Comments comment) {
+		return commentsRepository.save(comment);
 	}
 
 	public List<Comments> getComments() {
 		return commentsRepository.findAll();
 
 	}
-
-//public Optional<Comments> getCommentsById(int comment_id) {
-//	return commentsRepository.findById(comment_id);
-//
-//}
-
 	public Comments getCommentsByCommentId(int comment_id) {
 		return commentsRepository.findByCommentId(comment_id);
 
 	}
 
 	public Comments getCommentsByEmployeeId(int employee_id) {
-
 		return commentsRepository.findByEmpId(employee_id);
-
 	}
 
 	public Comments getCommentsByCandidateId(int candidate_id) {
-
 		return commentsRepository.findByCandidId(candidate_id);
-
 	}
 
-	public APIResponse updateComment(Comments comment, int comment_id) {
-		APIResponse apiResponse = new APIResponse();
+	public Comments updateComment(Comments comment, int comment_id) {
 		Optional<Comments> commentId = commentsRepository.findById(comment_id);
-
 		boolean present = commentId.isPresent();
+		Comments comm = null;
 		if (present == true) {
-
+			comm = commentId.get();
 			LocalDateTime now = LocalDateTime.now();
-			Comments comm = commentId.get();
 			comm.setComments(comment.getComments());
 			comm.setUpdatedDate(now);
 			comm.setResult(comment.getResult());
-			commentsRepository.save(comm);
-			apiResponse.setData("updated Successfully");
 		}
-		return apiResponse;
+		return commentsRepository.save(comm);
 	}
 
 	public void deleteComment(int comment_id) throws UserNotFoundException {
@@ -80,7 +60,6 @@ public class CommentServiceImpl implements CommentService{
 		} else {
 			throw new UserNotFoundException("comment id not found" + " " + comment_id);
 		}
-
 	}
 
 }
